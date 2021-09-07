bd49b48aee3061c3c874f533e5cb06753d9739b2
xRaWCrAzY <djdomas15@gmail.com>
2021-09-07 23:19:27 +0300

    Delete Kalėdinės kortelės WEB/KaleduKorteles directory

--- Kalėdinės kortelės WEB/KaleduKorteles/main/templatetags/samestr.py
+++
@@ -1,9 +0,0 @@
-from django import template
-register = template.Library()
-
-@register.filter
-def samestr(str1, str2):
-    if str(str1) == str(str2):
-        return True
-    else:
-         return False
